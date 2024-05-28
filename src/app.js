59e089dfd3677ed3f013eb8fce79d5b56f91aa6b
Gerrysan10 <20460135@colima.tecnm.mx>
2024-05-28 14:16:56 -0600

    fin

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.routes.js'
 import dataRoutes from './routes/data.routes.js';
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://client-fit-g-life.vercel.app'
 }))
 
 app.use(morgan('dev'));
